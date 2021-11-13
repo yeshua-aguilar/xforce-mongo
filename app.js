9c3013b5dc2a7798637bd3ca1c8cd1b76e3e8914
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2021-11-13 10:47:16 -0500

    13/11/2021

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 
 //importar imagenes
-app.use(express.static(path.join(__dirname, 'public/')));
+//app.use(express.static(path.join(__dirname, 'public/')));
 
 
 // REESCRIBIR RUTAS
