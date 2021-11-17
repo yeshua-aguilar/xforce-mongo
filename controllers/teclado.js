42757f7e2d9927903c98ba379b97988a59f4c58f
carloslorap <74977007@certus.edu.pe>
2021-11-17 18:52:45 -0500

    HOLAA

--- controllers/teclado.js
+++ controllers/teclado.js
@@ -13,6 +13,47 @@ var controller = {
             ).catch(
                 error => console.log(error)
             )
+    },
+    
+    form: function (req, res) {
+        console.log("-------------------");
+        console.log("ENTRANDO A LA FUNCION FORM");
+        console.log("id:" + req.params.id);
+        if (req.params.id == 1) {
+            var teclado = {}
+            teclado.nombre = "";
+            teclado.categoria = "";
+            teclado.marca = "";
+            teclado.precio = "";
+            res.render('teclado_form', { tecladoForm:teclado });
+        }
+
+
+    },
+
+ save: function (req, res){
+    console.log("-------------------");
+    console.log("ENTRADA A LA FUNCION GUARDAR");
+    console.log(req.body);
+    if(req.body.nombre){
+        var teclado = {}
+        teclado.nombre = req.body.nombre;
+        teclado.IDPRODUCTO = req.body.IDPRODUCTO;
+        teclado.PRECIO = req.body.PRECIO;
+        console.log(teclado);
+        db.collection("teclado").insertOne(teclado).then(
+
+            ()=>{
+                res.redirect('/views/teclado/list');
+            }
+        ).catch(
+            error => console.log(error)
+        )
     }
+}
+
+
+
+
 }
 module.exports = controller;
--- routes/teclado.js
+++ routes/teclado.js
@@ -6,6 +6,6 @@ var router = express.Router();
 
 // RUTAS PARA PRODUCTO
 router.get('/views/teclado/list', TecladoController.listar);
-
-
+router.get('/views/teclado/form/:id', TecladoController.form);//abrir formularia
+router.post('/teclado/save',TecladoController.save);
 module.exports = router;
