f298a62102964c284ac147527c15b849dee9eb0e
Kventura <81574011+keni-afk@users.noreply.github.com>
2021-11-19 02:50:53 -0500

    ms

--- controllers/mouse.js
+++ controllers/mouse.js
@@ -13,6 +13,37 @@ var controller = {
             ).catch(
                 error => console.log(error)
             )
+    },
+    form: function (req, res) {
+        console.log("-------------------");
+        console.log("ENTRANDO A LA FUNCION FORM");
+        console.log("id:" + req.params.id);
+        if (req.params.id == 1) {
+            var mouse = {}
+            mouse.nombre = "";
+            mouse.precio = "";
+            res.render('mouse_form', { mouseForm: mouse });
+        }
+
+
+    },
+    save: function (req, res){
+        console.log("-------------------");
+        console.log("ENTRADA A LA FUNCION GUARDAR");
+        console.log(req.body);
+        if(req.body.nombre){
+            var mouse = {}
+            mouse.nombre= req.body.nombre;
+            mouse.PRECIO = req.body.PRECIO;
+            console.log(mouse);
+            db.collection("mouse").insertOne(mouse).then(
+                ()=>{
+                    res.redirect('/views/mouse/list');
+                }
+            ).catch(
+                error => console.log(error)
+            )
+        }
     }
 }
 module.exports = controller;
